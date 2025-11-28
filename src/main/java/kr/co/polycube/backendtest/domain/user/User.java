ad66ab8b780fe9dc809bf5906ba9682333ae1365
Kiki1875b <kevinheo0413@gmail.com>
2025-11-28 14:26:08 +0900

    feat(user): added basic user entity

---
+++ src/main/java/kr/co/polycube/backendtest/domain/user/User.java
@@ -0,0 +1,29 @@
+package kr.co.polycube.backendtest.domain.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column
+  private String name;
+
+  public User(String name){
+    this.name = name;
+  }
+}
